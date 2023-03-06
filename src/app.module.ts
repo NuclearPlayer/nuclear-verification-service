ec381d6795dcd9cd9474636a9636fdf88d1af740
nukeop <12746779+nukeop@users.noreply.github.com>
2023-03-06 01:45:19 +0100

    Reorganize module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
-import { StreamMappingsController } from './stream-mappings/stream-mappings.controller';
 import { StreamMappingsModule } from './stream-mappings/stream-mappings.module';
-import { StreamMappingsService } from './stream-mappings/stream-mappings.service';
 
 @Module({
-  imports: [ConfigModule.forRoot(), StreamMappingsModule],
-  controllers: [StreamMappingsController],
-  providers: [StreamMappingsService],
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+    StreamMappingsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
